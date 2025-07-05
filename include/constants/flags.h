4b2f9ad0876159581c8426e3b2087fd232ab419c
Denscep <dennistuefekci@Icloud.com>
2025-07-05 14:48:31 +0200

    Fix devnav

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -43,11 +43,11 @@
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
-#define FLAG_MODERN_EXPSHARE    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
-#define FLAG_UNUSED_0x023    0x23 // Unused Flag
-#define FLAG_UNUSED_0x024    0x24 // Unused Flag
+#define FLAG_MODERN_EXPSHARE 0x20
+#define FLAG_UNUSED_0x021    0x21 // DEXNAV (now in use)
+#define FLAG_UNUSED_0x022    0x22 // DEXNAV
+#define FLAG_UNUSED_0x023    0x23 // DEXNAV
+#define FLAG_UNUSED_0x024    0x24 // DEXNAV
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
